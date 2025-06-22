2656f5ea320e39da117555d0f7a12fc0b2cbf12b
payam <payam.shr@yahoo.com>
2025-06-23 01:45:18 +0200

    header change

--- services/axiosConfig.ts
+++ services/axiosConfig.ts
@@ -5,6 +5,7 @@ const api = axios.create({
   baseURL:process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001/', 
   headers: {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
   },
 });
 
