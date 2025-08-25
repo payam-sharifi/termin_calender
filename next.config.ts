58c562dd39dddae97627c96256a3d2466a9d8089
payam <payam.shr@yahoo.com>
2025-08-25 22:54:13 +0200

    config change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  generateBuildId: async () => {
+    return 'build-' + Date.now(); 
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*.{js,css}',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
