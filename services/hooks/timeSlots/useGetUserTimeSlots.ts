a9196df90cdce58004af2cff32a1456d322d42cd
payam <payam.shr@yahoo.com>
2025-09-04 16:42:39 +0330

    fix type error for build

--- services/hooks/timeSlots/useGetUserTimeSlots.ts
+++ services/hooks/timeSlots/useGetUserTimeSlots.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { getUserTimeSlots } from "@/services/timeSlotsApi/userTimeSlots";
 import { UserTimeSlotQuery } from "@/services/timeSlotsApi/userTimeSlots.types";
 
@@ -6,7 +6,7 @@ export const useGetUserTimeSlots = (params: UserTimeSlotQuery) => {
   return useQuery({
     queryKey: ["user-time-slots", params],
     queryFn: () => getUserTimeSlots(params),
-    keepPreviousData: true,
+    placeholderData: keepPreviousData,
   });
 };
 
