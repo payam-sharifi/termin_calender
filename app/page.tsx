05f28b8243278af0e877bdb056772051fffd50aa
payam <payam.shr@yahoo.com>
2026-01-25 17:00:43 +0100

    kill sw

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,16 @@ import { useEffect } from "react";
 
 export default function Home() {
 const router=useRouter()
-
+useEffect(() => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      for (let registration of registrations) {
+        registration.unregister();
+        console.log('Service Worker unregistered');
+      }
+    });
+  }
+}, []);
 
 useEffect(()=>{
 
