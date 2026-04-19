379e372010efd4c943a433fdbfa7b1f2c2d35876
payam <payam.shr@yahoo.com>
2026-04-19 18:06:21 +0200

    past dates color change

--- components/Datapicker.tsx
+++ components/Datapicker.tsx
@@ -9,6 +9,14 @@ import { de } from "date-fns/locale/de";
 // Register German locale
 registerLocale("de", de);
 
+function dayClassNameForPast(date: Date): string {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d < today ? "german-datepicker-day--past" : "";
+}
+
 type Props = {
   selected: Date | null;
   onChange: (date: Date | null) => void;
@@ -70,6 +78,7 @@ export default function GermanDatePicker({
             minDate={minDate}
             filterDate={filterDate}
             className="mobile-datepicker"
+            dayClassName={dayClassNameForPast}
           />
         </div>
       )}
